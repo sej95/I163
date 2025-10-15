1c0ec45351a0f23bdd9fa11126f011cc77c5ee75
sej95 <144600333+sej95@users.noreply.github.com>
2025-10-15 09:49:07 +0800

    Update electron.vite.config.mjs

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -134,6 +134,12 @@ export default defineConfig(async ({ mode }) => {
         port: devPort,
         // 代理
         proxy: {
+          "/music_proxy/music": {
+            target: "https://mc.alger.fun",
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/music_proxy\/music/, "/music_proxy/music"),
+            secure: false, // 如果目标是 HTTPS，可能需要设置为 false
+          },
           "/api": {
             target: `http://${getEnv("MAIN_VITE_SERVER_HOST")}:${serverPort}`,
             changeOrigin: true,
