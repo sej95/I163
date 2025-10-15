d778fce1ff6b256b6f2ead655dbf2cc23c18f07a
sej95 <144600333+sej95@users.noreply.github.com>
2025-10-15 11:40:28 +0800

    Update electron.vite.config.mjs

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -134,12 +134,6 @@ export default defineConfig(async ({ mode }) => {
          port: devPort,
          // 代理
          proxy: {
-    '/api/song/url': {
-    target: 'http://localhost:11451', // 你的本地服务
-    changeOrigin: true,
-    rewrite: path => path.replace(/^\/api\/song\/url/, '/song/url')
-  },
-               
     "/api/music_proxy": {
       target: "http://mc.alger.fun",
       changeOrigin: true,
